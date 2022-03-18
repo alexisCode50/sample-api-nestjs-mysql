0c6ecfbc9b8bdd4c17798917f3c601f3436702fd
alexisCode50 <skrillex.ja14@gmail.com>
2022-03-17 21:54:43 -0600

    crud de bookmarks con testing(e2e)

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -6,10 +6,7 @@ import { Test } from '@nestjs/testing';
 import * as pactum from 'pactum';
 import { AppModule } from '../src/app.module';
 import { AuthDto } from '../src/auth/dto';
-// import {
-//   CreateBookmarkDto,
-//   EditBookmarkDto,
-// } from '../src/bookmark/dto';
+import { CreateBookmarkDto, EditBookmarkDto, } from '../src/bookmark/dto';
 import { PrismaService } from '../src/prisma/prisma.service';
 import { EditUserDto } from '../src/user/dto';
 
@@ -171,18 +168,124 @@ describe('App e2e', () => {
 
   });
 
-  // describe('Bookmarks', () => {
+  describe('Bookmarks', () => {
 
-  //   describe('Create Bookmark', () => { });
+    describe('Get empty bookmarks', () => {
 
-  //   describe('Get Bookmarks', () => { });
+      it('should get bookmarks', () => {
+        return pactum
+          .spec()
+          .get('/bookmarks')
+          .withHeaders({
+            Authorization: 'Bearer $S{userAt}',
+          })
+          .expectStatus(200)
+          .expectBody([]);
+      });
 
-  //   describe('Get Bookmark by id', () => { });
+    });
 
-  //   describe('Edit Bookmark by id', () => { });
+    describe('Create bookmark', () => {
 
-  //   describe('Delete Bookmark by id', () => { });
+      const dto: CreateBookmarkDto = {
+        title: 'First Bookmark',
+        link: 'https://www.youtube.com/watch?v=d6WC5n9G_sM',
+      };
 
-  // });
+      it('should create bookmark', () => {
+        return pactum
+          .spec()
+          .post('/bookmarks')
+          .withHeaders({
+            Authorization: 'Bearer $S{userAt}',
+          })
+          .withBody(dto)
+          .expectStatus(201)
+          .stores('bookmarkId', 'id');
+      });
+
+    });
+
+    describe('Get bookmarks', () => {
+
+      it('should get bookmarks', () => {
+        return pactum
+          .spec()
+          .get('/bookmarks')
+          .withHeaders({
+            Authorization: 'Bearer $S{userAt}',
+          })
+          .expectStatus(200)
+          .expectJsonLength(1);
+      });
+
+    });
+
+    describe('Get bookmark by id', () => {
+
+      it('should get bookmark by id', () => {
+        return pactum
+          .spec()
+          .get('/bookmarks/{id}')
+          .withPathParams('id', '$S{bookmarkId}')
+          .withHeaders({
+            Authorization: 'Bearer $S{userAt}',
+          })
+          .expectStatus(200)
+          .expectBodyContains('$S{bookmarkId}');
+      });
+
+    });
+
+    describe('Edit bookmark by id', () => {
+
+      const dto: EditBookmarkDto = {
+        title: 'Kubernetes Course - Full Beginners Tutorial (Containerize Your Apps!)',
+        description: 'Learn how to use Kubernetes in this complete course. Kubernetes makes it possible to containerize applications and simplifies app deployment to production.',
+      };
+
+      it('should edit bookmark', () => {
+        return pactum
+          .spec()
+          .patch('/bookmarks/{id}')
+          .withPathParams('id', '$S{bookmarkId}')
+          .withHeaders({
+            Authorization: 'Bearer $S{userAt}',
+          })
+          .withBody(dto)
+          .expectStatus(200)
+          .expectBodyContains(dto.title)
+          .expectBodyContains(dto.description);
+      });
+
+    });
+
+    describe('Delete bookmark by id', () => {
+
+      it('should delete bookmark', () => {
+        return pactum
+          .spec()
+          .delete('/bookmarks/{id}')
+          .withPathParams('id', '$S{bookmarkId}')
+          .withHeaders({
+            Authorization: 'Bearer $S{userAt}',
+          })
+          .expectStatus(204);
+      });
+
+      it('should get empty bookmarks', () => {
+        return pactum
+          .spec()
+          .get('/bookmarks')
+          .withHeaders({
+            Authorization: 'Bearer $S{userAt}',
+          })
+          .expectStatus(200)
+          .expectJsonLength(0);
+      });
+
+    });
+
+  });
 
 });
